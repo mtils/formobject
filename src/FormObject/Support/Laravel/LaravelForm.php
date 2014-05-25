eaf1db955b2ddd7be042a444109dc57075fb22f6
Michael Tils <mtils@web-utils.de>
2014-05-25 10:52:32 +0200

    * Added Titles as custom attributes for better errormessages

--- src/FormObject/Support/Laravel/LaravelForm.php
+++ src/FormObject/Support/Laravel/LaravelForm.php
@@ -1,8 +1,10 @@
 <?php namespace FormObject\Support\Laravel;
 
+use DomainException;
 use FormObject\Form;
 use FormObject\Field\HiddenField;
 use \App;
+use Illuminate\Validation\Validator;
 
 class LaravelForm extends Form{
 
@@ -22,5 +24,23 @@ class LaravelForm extends Form{
 
     protected function createValidatorAdapter($validator){
         return new ValidatorAdapter($this, $validator);
+    }
+
+    public function setValidator($validator){
+
+        if(!$validator instanceof Validator){
+            throw new DomainException('LaravelForm Validators have to be Illuminate\Validation\Validator');
+        }
+        $validator->setAttributeNames($this->buildAttributeNames());
+
+        parent::setValidator($validator);
+    }
+
+    protected function buildAttributeNames(){
+        $attributeNames = array();
+        foreach($this->getDataFields() as $field){
+            $attributeNames[$field->getName()] = $field->getTitle();
+        }
+        return $attributeNames;
     }
 }
