955944994e23f792e295b02c3f2206d5e9909eba
Michael Tils <mtils@web-utils.de>
2016-05-31 13:12:19 +0200

    Added inlinejs method

--- src/FormObject/Field/HtmlField.php
+++ src/FormObject/Field/HtmlField.php
@@ -6,6 +6,20 @@ use FormObject\Attributes;
 
 class HtmlField extends RichTextField
 {
+
+    protected $inlineJsEnabled = false;
+
+    public function enableInlineJs($enabled=true)
+    {
+        $this->inlineJsEnabled = $enabled;
+        return $this;
+    }
+
+    public function isInlineJsEnabled()
+    {
+        return $this->inlineJsEnabled;
+    }
+
     protected function addRuleCssClassesIfNotAdded()
     {
         parent::addRuleCssClassesIfNotAdded();
